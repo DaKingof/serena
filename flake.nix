b6cf843ab992545d5a294e38c8be9deccd360391
DaKingof <thedjknownasez@hotmail.com>
2025-08-31 18:30:42 -0400

    Revert "fix(nix): link gcc correctly in flake.nix"
    This reverts commit e9d9a1b55faab807c849512bc081ed2bd4f07f36.

--- flake.nix
+++ flake.nix
@@ -82,13 +82,16 @@
         default = pkgs.mkShell {
           packages = [
             python
-            pkgs.gcc
+            pkgs.gccStdenv
             pkgs.uv
           ];
           env =
             {
               UV_PYTHON_DOWNLOADS = "never";
               UV_PYTHON = python.interpreter;
+	      CC  = "${pkgs.gccStdenv}/bin/gcc";
+    	      CXX = "${pkgs.gccStdenv}/bin/g++";
+    	      LD  = "${pkgs.gccStdenv}/bin/gcc";
             }
             // lib.optionalAttrs pkgs.stdenv.isLinux {
               LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
